1d82ce583ddcbe986e785b70b5d245950b4ee00c
Tony <izzi.tony@gmail.com>
2023-06-30 10:19:30 +0200

    feat:0003/Ajout des verification des champs

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var fieldPassword: UITextField!
     @IBOutlet weak var fieldLogin: UITextField!
+    @IBOutlet weak var switchNewslette: UISwitch!
     @IBOutlet weak var hidePasswordImage: UIImageView!
     
     override func viewDidLoad() {
@@ -34,8 +35,6 @@ class ViewController: UIViewController {
         
     }
     
-    
-    
     // Observer du Button pour afficher/Cacher le Password
     @IBAction func SwitchHidePassword() {
         if fieldPassword.isSecureTextEntry {
@@ -52,6 +51,49 @@ class ViewController: UIViewController {
     }
 
     
+    @IBAction func tapOnLogin() {
+        let fLogin = fieldLogin.text!
+        let fPassword = fieldPassword.text!
+        let sNews = switchNewslette.isOn
+        
+        
+        if(fLogin.isEmpty){
+            alertMessage(title: "Error", message: "Pas de login entrée")
+            
+        }else if(fPassword.isEmpty){
+            alertMessage(title: "Error", message: "Pas de password entrée")
+            
+        }else if(!fLogin.contains("@")){
+            alertMessage(title: "Error", message: "Ce n'est pas une adresse mail valide")
+            
+        }else if(fPassword.count < 4){
+            alertMessage(title: "Error", message: "Password pas assez fort")
+            
+        }else{
+            let message: String
+            if(sNews){
+                message = "Vous vous ete inscrit à la newsletter"
+            }else{
+                message = "Vous n'etepas inscrit à la newsletter"
+            }
+            
+            alertMessage(title: "Bievenue \(fLogin)", message: message )
+        }
+        
+        
+    }
+    
+    
+    
+    func alertMessage(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in print("TOUCH OK")
+        }))
+
+        present(alert, animated: true, completion: nil)
+        
+    }
     
 }
 
