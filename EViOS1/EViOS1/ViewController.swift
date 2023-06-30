075b90bef702dc3ca90121ef59da9be91cfe5d69
Tony <izzi.tony@gmail.com>
2023-06-30 09:57:45 +0200

    feat:0002/Implementation de la securité du FieldText Password

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -10,13 +10,48 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var viewOfUserIcon: UIView!
     
+    @IBOutlet weak var fieldPassword: UITextField!
+    @IBOutlet weak var fieldLogin: UITextField!
+    @IBOutlet weak var hidePasswordImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        //Mettre la view en cercle derrier l'image User
         viewOfUserIcon.layer.cornerRadius = 75
+        
+        
+        //Exo2
+        fieldLogin.keyboardType = .emailAddress
+        fieldLogin.textContentType = .emailAddress
+        
+        fieldPassword.isSecureTextEntry = true
+        fieldPassword.keyboardType = .default
+        
+        // Désactivation du clavier en cliquant dans la View
+        let tapOnView = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapOnView)
+        
+        
+    }
+    
+    
+    
+    // Observer du Button pour afficher/Cacher le Password
+    @IBAction func SwitchHidePassword() {
+        if fieldPassword.isSecureTextEntry {
+            fieldPassword.isSecureTextEntry = false
+            hidePasswordImage.image = UIImage(named: "eye_off_icon")
+            
+        }else{
+            fieldPassword.isSecureTextEntry = true
+            hidePasswordImage.image = UIImage(named: "eye_on_icon")
+        }
+            
+        
+        
     }
 
-
+    
+    
 }
 
