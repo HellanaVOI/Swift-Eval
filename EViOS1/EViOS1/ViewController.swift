b684e5b48794df65ac064387faee695c7e00cc96
Tony <izzi.tony@gmail.com>
2023-06-30 09:13:21 +0200

    feat:0001/Mise en place de l'UI

---
+++ EViOS1/EViOS1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  EViOS1
+//
+//  Created by Student09 on 30/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var viewOfUserIcon: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        viewOfUserIcon.layer.cornerRadius = 75
+    }
+
+
+}
+
